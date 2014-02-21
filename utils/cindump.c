751dc05ea8b8e0ec4cb1545f796d67b6eb8ff3fb
FastCCDUser <fccduser@gmail.com>
2014-02-21 15:48:58 -0800

    Updated to correct bind address

--- utils/cindump.c
+++ utils/cindump.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]){
 
   fprintf(stderr, "\n\n\n\n");
 
-  if(cin_init_data_port(&port, "10.23.5.1", 0, NULL, 0, 1000)){
+  if(cin_init_data_port(&port, "10.0.5.55", 0, NULL, 0, 1000)){
     exit(1);
   }
 
