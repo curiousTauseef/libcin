be1dc733e096c02170bdb7d024fd1d570e561d0e
Stuart Wilkins <swilkins@bnl.gov>
2014-04-06 10:23:05 -0400

    Modified to take new callback centered library

--- utils/cindump.c
+++ utils/cindump.c
@@ -17,48 +17,69 @@ void int_handler(int dummy){
   exit(0);
 }
 
+void write_image(cin_data_frame_t *frame)
+{
+  char filename[256];
+  TIFF *tfp;
+  //if(output_path){
+  //  sprintf(filename, "%s/frame%08d.tif", output_path, frame_number);
+  //} else {
+  sprintf(filename, "frame%08d.tif", frame->number);
+  //}
+
+  tfp = TIFFOpen(filename, "w");
+
+  TIFFSetField(tfp, TIFFTAG_IMAGEWIDTH, CIN_DATA_FRAME_WIDTH);
+  TIFFSetField(tfp, TIFFTAG_BITSPERSAMPLE, 16);
+  TIFFSetField(tfp, TIFFTAG_SAMPLESPERPIXEL, 1);
+  TIFFSetField(tfp, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+  TIFFSetField(tfp, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
+  TIFFSetField(tfp, TIFFTAG_ORIENTATION, ORIENTATION_BOTLEFT);
+
+  uint16_t *p = frame->data;
+  int j;
+  for(j=0;j<CIN_DATA_FRAME_HEIGHT;j++){
+    TIFFWriteScanline(tfp, p, j, 0);
+    p += CIN_DATA_FRAME_WIDTH;
+  }
+
+  TIFFClose(tfp);
+  
+  free(frame->data); // Very important!
+
+  return;
+}
+
+void allocate_image(cin_data_frame_t *frame)
+{
+  // Allocate memory
+  while(!(frame->data = malloc(CIN_DATA_FRAME_WIDTH * CIN_DATA_FRAME_HEIGHT * sizeof(uint16_t))))
+  {
+    sleep(1);
+    fprintf(stderr, "Failed to allocate memory ... sleeping to wait for data.\n");
+  }
+  return;
+}
+
 int main(int argc, char *argv[]){
 
-  /* Command line options */
-  int c;
-
-  /* For writing out */
-  TIFF *tfp;
-  int j;
-  uint16_t *p;
-  FILE *fp = NULL;
-  char filename[256];
-  struct cin_port port;
-
-  uint16_t buffer[CIN_DATA_FRAME_WIDTH * CIN_DATA_FRAME_HEIGHT];
-  uint16_t frame_number;
-
   char *output_path = NULL;
-  int show_monitor = 1;
-  int tiff_output = 1;
-  int chunk_size = 1;
+  int show_monitor = 0;
 
-  while((c = getopt(argc, argv, "hrmd:c")) != -1){
+  int c;
+  while((c = getopt(argc, argv, "hmd:")) != -1){
     switch(c){
-      case 'r':
-        tiff_output =0;
-        break;
       case 'm':
-        show_monitor = 0;
+        show_monitor = 1;
         break;
       case 'd':
         output_path = optarg;
         break;
-      case 'c':
-        chunk_size = atoi(optarg);
-        break;
       case 'h':
       default:
         fprintf(stderr,"cindump : Dump data from CIN\n\n");
-        fprintf(stderr,"\t -r : Write files in raw (uint16_t format)\n");
-        fprintf(stderr,"\t -m : Supress status monitor\n");
+        fprintf(stderr,"\t -m : Show status monitor\n");
         fprintf(stderr,"\t -d : Specify output directory\n");
-        fprintf(stderr,"\t -c : Chunk size (for raw format)\n");
         fprintf(stderr,"\n");
         exit(1);
         break;
@@ -67,12 +88,16 @@ int main(int argc, char *argv[]){
 
   fprintf(stderr, "\n\n\n\n");
 
-  if(cin_data_init_port(&port, NULL, 0, NULL, 0, 1000)){
+  struct cin_port port;
+  if(cin_data_init_port(&port, NULL, 0, NULL, 0, 1000))
+  {
     exit(1);
   }
 
   /* Start the main routine */
-  if(cin_data_init(CIN_DATA_MODE_PUSH_PULL, 20000, 2000)){
+  if(cin_data_init(CIN_DATA_MODE_CALLBACK, 20000, 2000,
+                   allocate_image, write_image))
+  {
     exit(1);
   }
 
@@ -85,49 +110,7 @@ int main(int argc, char *argv[]){
   } 
 
   while(1){
-    // Load the buffer
-    cin_data_load_frame(buffer, &frame_number);
-
-    if(tiff_output){
-      if(output_path){
-        sprintf(filename, "%s/frame%08d.tif", output_path, frame_number);
-      } else {
-        sprintf(filename, "frame%08d.tif", frame_number);
-      }
-
-      tfp = TIFFOpen(filename, "w");
-
-      TIFFSetField(tfp, TIFFTAG_IMAGEWIDTH, CIN_DATA_FRAME_WIDTH);
-      TIFFSetField(tfp, TIFFTAG_BITSPERSAMPLE, 16);
-      TIFFSetField(tfp, TIFFTAG_SAMPLESPERPIXEL, 1);
-      TIFFSetField(tfp, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-      TIFFSetField(tfp, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
-      TIFFSetField(tfp, TIFFTAG_ORIENTATION, ORIENTATION_BOTLEFT);
-
-      p = buffer;
-      for(j=0;j<CIN_DATA_FRAME_HEIGHT;j++){
-        TIFFWriteScanline(tfp, p, j, 0);
-        p += CIN_DATA_FRAME_WIDTH;
-      }
-
-      TIFFClose(tfp);
-
-    } else {
-      if(!fp || !(frame_number % chunk_size)){
-        if(fp){
-          fclose(fp);
-        }
-        if(output_path){
-          sprintf(filename, "%s/frame%08d.bin", output_path, frame_number);
-        } else {
-          sprintf(filename, "frame%08d.bin", frame_number);
-        }
-        fp = fopen(filename, "w");
-      }
-      fwrite(buffer, sizeof(uint16_t),
-             CIN_DATA_FRAME_HEIGHT * CIN_DATA_FRAME_WIDTH, fp);
-    }
-
+    sleep(10);
   }
 
   cin_data_wait_for_threads();
