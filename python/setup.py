c9a59d310d24d5bb420e82ca222d99acd9d8abfd
Stuart B. Wilkins <swilkins@bnl.gov>
2015-11-27 09:24:33 -0500

    Autogenerated pages Fri Nov 27 09:24:33 EST 2015

--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-data_module = Extension('cindata',
-                        sources = ['cindata_module.c'])
-
-setup (name = 'pycin', 
-       version = '1.0',
-       description = 'Python bindings for libcin',
-       ext_modules = [data_module])
