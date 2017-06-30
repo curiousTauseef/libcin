300d371f41f1d737deab9bb388ef51f0b129c258
Stuart B. Wilkins <swilkins@bnl.gov>
2017-06-30 19:18:04 -0400

    ENH : Added git printout to smoketest

--- test/smoketest.c
+++ test/smoketest.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "cin.h"
+#include "version.h"
 
 int main(int argc, char *argv[]){
   cin_set_debug_print(1);
   cin_set_error_print(1);
+  DEBUG_PRINT("Git SHA           = %s\n", cin_build_git_sha);
+  DEBUG_PRINT("Git Time          = %s\n", cin_build_git_time);
+  DEBUG_PRINT("Git Build Version = %s\n", cin_build_version);
   exit(0);
 }
