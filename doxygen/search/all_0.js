26eed59af8fb5ea4f9622951082b6006496c10d7
Travis CI <travis@travis-ci.org>
2017-07-04 15:53:27 +0000

    Deploy code docs to GitHub Pages Travis build: 108
    Commit: 8bfbd9dd6c6f9309a59eae7286a37b3ec0b5d53f

--- doxygen/search/all_0.js
+++ doxygen/search/all_0.js
@@ -13,7 +13,7 @@ var searchData=
   ['cin_5fctl_5fstream_5fwrite',['cin_ctl_stream_write',['../group__cin__ctl.html#ga0331c06356498297810ec163535eeb1d',1,'cin_ctl_stream_write(cin_ctl_t *cin, char *val, int size):&#160;control.c'],['../group__cin__ctl.html#ga0331c06356498297810ec163535eeb1d',1,'cin_ctl_stream_write(cin_ctl_t *cin, char *val, int size):&#160;control.c']]],
   ['cin_5fctl_5fwrite',['cin_ctl_write',['../group__cin__ctl.html#gac9b282b5280a34b0abb9ea0dcc6e3308',1,'cin_ctl_write(cin_ctl_t *cin, uint16_t reg, uint16_t val, int wait):&#160;control.c'],['../group__cin__ctl.html#gac9b282b5280a34b0abb9ea0dcc6e3308',1,'cin_ctl_write(cin_ctl_t *cin, uint16_t reg, uint16_t val, int wait):&#160;control.c']]],
   ['cin_5fctl_5fwrite_5fwith_5freadback',['cin_ctl_write_with_readback',['../group__cin__ctl.html#ga15ae1265f7abc5a08ca4560c007a92b0',1,'cin_ctl_write_with_readback(cin_ctl_t *cin, uint16_t reg, uint16_t val):&#160;control.c'],['../group__cin__ctl.html#ga15ae1265f7abc5a08ca4560c007a92b0',1,'cin_ctl_write_with_readback(cin_ctl_t *cin, uint16_t reg, uint16_t val):&#160;control.c']]],
-  ['cin_5fdata',['cin_data',['../structcin__data.html',1,'']]],
+  ['cin_5fdata',['cin_data',['../structcin__data.html',1,'cin_data'],['../group__cin__data.html',1,'(Global Namespace)']]],
   ['cin_5fdata_5fcallbacks',['cin_data_callbacks',['../structcin__data__callbacks.html',1,'']]],
   ['cin_5fdata_5fframe',['cin_data_frame',['../structcin__data__frame.html',1,'']]],
   ['cin_5fdata_5finit',['cin_data_init',['../cin_8h.html#a128d41b42ffc9da69892f05861929e1d',1,'data.c']]],
