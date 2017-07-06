92a8b786ac8224f2499926f132df84756c77a368
Travis CI <travis@travis-ci.org>
2017-07-06 18:32:48 +0000

    Deploy code docs to GitHub Pages Travis build: 119
    Commit: 08c22c7e0f197d33f93df32b1ae3f42b457396e3

--- doxygen/search/functions_0.js
+++ doxygen/search/functions_0.js
@@ -6,6 +6,9 @@ var searchData=
   ['cin_5fctl_5fstream_5fwrite',['cin_ctl_stream_write',['../group__cin__ctl.html#ga0331c06356498297810ec163535eeb1d',1,'cin_ctl_stream_write(cin_ctl_t *cin, char *val, int size):&#160;control.c'],['../group__cin__ctl.html#ga0331c06356498297810ec163535eeb1d',1,'cin_ctl_stream_write(cin_ctl_t *cin, char *val, int size):&#160;control.c']]],
   ['cin_5fctl_5fwrite',['cin_ctl_write',['../group__cin__ctl.html#gac9b282b5280a34b0abb9ea0dcc6e3308',1,'cin_ctl_write(cin_ctl_t *cin, uint16_t reg, uint16_t val, int wait):&#160;control.c'],['../group__cin__ctl.html#gac9b282b5280a34b0abb9ea0dcc6e3308',1,'cin_ctl_write(cin_ctl_t *cin, uint16_t reg, uint16_t val, int wait):&#160;control.c']]],
   ['cin_5fctl_5fwrite_5fwith_5freadback',['cin_ctl_write_with_readback',['../group__cin__ctl.html#ga15ae1265f7abc5a08ca4560c007a92b0',1,'cin_ctl_write_with_readback(cin_ctl_t *cin, uint16_t reg, uint16_t val):&#160;control.c'],['../group__cin__ctl.html#ga15ae1265f7abc5a08ca4560c007a92b0',1,'cin_ctl_write_with_readback(cin_ctl_t *cin, uint16_t reg, uint16_t val):&#160;control.c']]],
-  ['cin_5fdata_5fframestore_5ftrigger',['cin_data_framestore_trigger',['../cin_8h.html#aadeeae6d9adf79a8a661bb1e668967e5',1,'data.c']]],
+  ['cin_5fdata_5fframestore_5fdisable',['cin_data_framestore_disable',['../cin_8h.html#a4bf11587b2e32826e28ada5c2f656c63',1,'data.c']]],
+  ['cin_5fdata_5fframestore_5fskip',['cin_data_framestore_skip',['../cin_8h.html#a4c4ccc7ec34ae559c28622d6b48017e8',1,'cin.h']]],
+  ['cin_5fdata_5fframestore_5ftrigger',['cin_data_framestore_trigger',['../cin_8h.html#a66d8e0e63f2bb83a98177a828f3061b9',1,'data.c']]],
+  ['cin_5fdata_5fget_5fframestore_5fcounter',['cin_data_get_framestore_counter',['../cin_8h.html#ab1ca9b95c7cfcd50a443156835cc6408',1,'data.c']]],
   ['cin_5fdata_5finit',['cin_data_init',['../cin_8h.html#a128d41b42ffc9da69892f05861929e1d',1,'data.c']]]
 ];
--- doxygen/search/groups_0.js
+++ doxygen/search/groups_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['cin_20control_20routines',['Cin Control Routines',['../group__cin__ctl.html',1,'']]],
-  ['cin_20data_20routines',['CIN Data Routines',['../group__cin__data.html',1,'']]]
+  ['cin_20data_20framestore_20functions',['CIN Data Framestore Functions',['../group__cin__data__framestore.html',1,'']]]
 ];
