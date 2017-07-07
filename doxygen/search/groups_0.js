0598f942b5c76dd8eee155614bb686f155334881
Travis CI <travis@travis-ci.org>
2017-07-07 13:34:26 +0000

    Deploy code docs to GitHub Pages Travis build: 181
    Commit: a243fdffacf29d6aaa8104f68247453fb47f8576

--- doxygen/search/groups_0.js
+++ doxygen/search/groups_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['cin_20control_20routines',['Cin Control Routines',['../group__cin__ctl.html',1,'']]],
+  ['cin_20control_20initialization_20routines',['Cin Control Initialization Routines',['../group__cin__ctl__init.html',1,'']]],
+  ['cin_20control_20read_2frwite_20routines',['Cin Control Read/Rwite Routines',['../group__cin__ctl__rw.html',1,'']]],
   ['cin_20data_20framestore_20functions',['CIN Data Framestore Functions',['../group__cin__data__framestore.html',1,'']]],
   ['cin_20data_20initialization_20routines',['CIN Data Initialization Routines',['../group__cin__data__init.html',1,'']]]
 ];
