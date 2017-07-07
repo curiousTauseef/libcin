232a520468d4f4af4d07ff8c25299cdacd8c13e9
Travis CI <travis@travis-ci.org>
2017-07-07 00:54:58 +0000

    Deploy code docs to GitHub Pages Travis build: 161
    Commit: 00783cfd227008faf5c34f2b589099d89f1bbebe

--- doxygen/search/pages_0.js
+++ doxygen/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['fifo',['fifo',['../structfifo.html',1,'']]],
   ['fastccd_20communication_20library_20_28libcin_29',['FastCCD Communication Library (libcin)',['../index.html',1,'']]]
 ];
--- doxygen/search/searchdata.js
+++ doxygen/search/searchdata.js
@@ -4,7 +4,8 @@ var indexSectionsWithContent =
   1: "cdf",
   2: "c",
   3: "c",
-  4: "c"
+  4: "c",
+  5: "f"
 };
 
 var indexSectionNames =
@@ -13,7 +14,8 @@ var indexSectionNames =
   1: "classes",
   2: "files",
   3: "functions",
-  4: "groups"
+  4: "groups",
+  5: "pages"
 };
 
 var indexSectionLabels =
@@ -22,6 +24,7 @@ var indexSectionLabels =
   1: "Classes",
   2: "Files",
   3: "Functions",
-  4: "Modules"
+  4: "Modules",
+  5: "Pages"
 };
 
