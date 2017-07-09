8466bf935c3a4f5c9d3c16be94d423dec110f498
Travis CI <travis@travis-ci.org>
2017-07-09 20:44:01 +0000

    Deploy code docs to GitHub Pages Travis build: 205
    Commit: 6eeaa2d5e855bb3160f54dd2894261c4e919bcb6

--- doxygen/search/groups_0.js
+++ doxygen/search/groups_0.js
@@ -1,7 +1,9 @@
 var searchData=
 [
+  ['cin_20control_20bias_20routines',['CIN Control Bias Routines',['../group__cin__ctl__bias.html',1,'']]],
   ['cin_20control_20initialization_20routines',['Cin Control Initialization Routines',['../group__cin__ctl__init.html',1,'']]],
   ['cin_20control_20read_2frwite_20routines',['Cin Control Read/Rwite Routines',['../group__cin__ctl__rw.html',1,'']]],
+  ['cin_20control_20timing_20routines',['CIN COntrol Timing Routines',['../group__cin__ctl__timing.html',1,'']]],
   ['cin_20data_20framestore_20functions',['CIN Data Framestore Functions',['../group__cin__data__framestore.html',1,'']]],
   ['cin_20data_20initialization_20routines',['CIN Data Initialization Routines',['../group__cin__data__init.html',1,'']]]
 ];
