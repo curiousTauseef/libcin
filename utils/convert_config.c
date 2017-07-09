d88524b7b9da44005b89a638f4f83d812632ef30
Stuart B. Wilkins <swilkins@bnl.gov>
2017-07-08 21:46:28 -0400

    ENH : Added config file parser

--- utils/convert_config.c
+++ utils/convert_config.c
@@ -5,9 +5,8 @@
 #include <string.h>
 #include "cin.h"
 
-uint16_t fcric_regs[] = {0x821D, 0x821E, 0x821F, 0x8001};
-uint16_t cin_timing_regs[] = {0x8200, 0x8201, 0x8001};
-
+uint16_t cin_fcric_regs[] = {0x821D, 0x821E, 0x821F, 0x8001};
+uint16_t cin_bias_timing_regs[] = {0x8200, 0x8201, 0x8001};
 
 char *strstrip(char *s)
 {
@@ -88,9 +87,9 @@ int parse_fcric_data(uint16_t vals[][2], int n_vals, uint16_t *pvals, int *n_pva
   int x = 0;
   for(i=0;i<156;i++){
     int j = i + 5;
-    if(vals[j][0] != fcric_regs[i % 4]){
+    if(vals[j][0] != cin_fcric_regs[i % 4]){
       fprintf(stderr, "!!! Error at line %d (%04X,%04X)\n", j,
-          vals[j][0], fcric_regs[i % 4]);
+          vals[j][0], cin_fcric_regs[i % 4]);
       return -1;
     } else {
       if(((i % 4) == 0) && (vals[j][1] != 0xA000)){
@@ -117,17 +116,17 @@ int parse_fcric_data(uint16_t vals[][2], int n_vals, uint16_t *pvals, int *n_pva
 
 }
 
-int parse_timing_data(uint16_t vals[][2], int n_vals, uint16_t *pvals, int *n_pvals)
+int parse_bias_timing_data(uint16_t vals[][2], int n_vals, uint16_t *pvals, int *n_pvals, uint16_t reg)
 {
   int i;
   int x = 0;
   for(i=0;i<n_vals-1;i++){
-    if(vals[i][0] != cin_timing_regs[i % 3]){
+    if(vals[i][0] != cin_bias_timing_regs[i % 3]){
       fprintf(stderr, "!!! Error at line %d (%04X,%04X)\n", i,
-          vals[i][0], fcric_regs[i % 3]);
+          vals[i][0], cin_fcric_regs[i % 3]);
       return -1;
     } else {
-      if(((i % 3) == 2) && (vals[i][1] != 0x0103)){
+      if(((i % 3) == 2) && (vals[i][1] != reg)){
         fprintf(stderr, "!!! Error at line %d (%04X vs %04X)\n", i, vals[i][1], 0x0103);
         return -1;
       } 
@@ -250,7 +249,14 @@ int main(int argc, char *argv[])
   }
 
   if(tflag){
-    if(parse_timing_data(vals, n_vals, pvals, &n_pvals))
+    if(parse_bias_timing_data(vals, n_vals, pvals, &n_pvals, 0x0103))
+    {
+      return 20;
+    }
+  }
+
+  if(bflag){
+    if(parse_bias_timing_data(vals, n_vals, pvals, &n_pvals, 0x0102))
     {
       return 20;
     }
