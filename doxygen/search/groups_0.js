e3c9b586addbbdd75e26d22b76eb49a91a70defa
Travis CI <travis@travis-ci.org>
2017-07-12 01:07:36 +0000

    Deploy code docs to GitHub Pages Travis build: 220
    Commit: 64023c2660131f9808823c3b76990e45be16e39f

--- doxygen/search/groups_0.js
+++ doxygen/search/groups_0.js
@@ -1,8 +1,11 @@
 var searchData=
 [
   ['cin_20control_20bias_20routines',['CIN Control Bias Routines',['../group__cin__ctl__bias.html',1,'']]],
+  ['cin_20fclk_20configuration_20routines',['CIN FCLK Configuration Routines',['../group__cin__ctl__fclk.html',1,'']]],
+  ['cin_20firmware_20upload_20routines',['CIN Firmware Upload Routines',['../group__cin__ctl__firmware.html',1,'']]],
   ['cin_20control_20initialization_20routines',['Cin Control Initialization Routines',['../group__cin__ctl__init.html',1,'']]],
   ['cin_20control_20read_2frwite_20routines',['Cin Control Read/Rwite Routines',['../group__cin__ctl__rw.html',1,'']]],
+  ['cin_20status_20routines',['CIN Status Routines',['../group__cin__ctl__status.html',1,'']]],
   ['cin_20control_20timing_20routines',['CIN COntrol Timing Routines',['../group__cin__ctl__timing.html',1,'']]],
   ['cin_20data_20framestore_20functions',['CIN Data Framestore Functions',['../group__cin__data__framestore.html',1,'']]],
   ['cin_20data_20initialization_20routines',['CIN Data Initialization Routines',['../group__cin__data__init.html',1,'']]]
