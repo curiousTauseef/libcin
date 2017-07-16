0f69ab0022bb15dee4c4d962862d7bf0dd7fd751
Travis CI <travis@travis-ci.org>
2017-07-16 00:43:24 +0000

    Deploy code docs to GitHub Pages Travis build: 245
    Commit: ccedf8f9643c431bedb726cfb6433e493d961be4

--- doxygen/search/all_2.js
+++ doxygen/search/all_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['fclk_5ffreq',['fclk_freq',['../structcin__config__timing.html#a938f05b2efad82f7e3f3828a39651f3d',1,'cin_config_timing']]],
   ['fclk_5ftime_5ftick',['fclk_time_tick',['../structcin__ctl.html#a74afc89ce2aa7f62bd1d6d84e25e0b02',1,'cin_ctl']]],
   ['fifo',['fifo',['../structfifo.html',1,'']]],
+  ['framestore',['framestore',['../structcin__config__timing.html#a894951a52cf7b8477b4b8a798ec9fb22',1,'cin_config_timing']]],
   ['fastccd_20communication_20library_20_28libcin_29',['FastCCD Communication Library (libcin)',['../index.html',1,'']]]
 ];
