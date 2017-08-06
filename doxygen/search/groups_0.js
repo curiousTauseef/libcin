1680407c93cafebae463950dc4ce60401d99c178
Travis CI <travis@travis-ci.org>
2017-08-06 12:49:19 +0000

    Deploy code docs to GitHub Pages Travis build: 282
    Commit: d61fd08ea5c2a5dd01f457f95d5e7e3667d868b8

--- doxygen/search/groups_0.js
+++ doxygen/search/groups_0.js
@@ -4,6 +4,7 @@ var searchData=
   ['cin_20fclk_20configuration_20routines',['CIN FCLK Configuration Routines',['../group__cin__ctl__fclk.html',1,'']]],
   ['cin_20firmware_20upload_20routines',['CIN Firmware Upload Routines',['../group__cin__ctl__firmware.html',1,'']]],
   ['cin_20control_20initialization_20routines',['Cin Control Initialization Routines',['../group__cin__ctl__init.html',1,'']]],
+  ['cin_20power_20routines',['Cin Power Routines',['../group__cin__ctl__power.html',1,'']]],
   ['cin_20control_20read_2frwite_20routines',['Cin Control Read/Rwite Routines',['../group__cin__ctl__rw.html',1,'']]],
   ['cin_20status_20routines',['CIN Status Routines',['../group__cin__ctl__status.html',1,'']]],
   ['cin_20control_20timing_20routines',['CIN COntrol Timing Routines',['../group__cin__ctl__timing.html',1,'']]],
